a4132e80021ebc0eb29d38450eb7b2e8c87b9463
Cody Sheridan <csheridan994@gmail.com>
2018-04-17 00:09:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue (object, key, value) {
+  return Object.assign({}, recipes, {prop: 1},  {prop2: 2});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var obj = { value: key }
+  var newObj = Object.assign({}, obj)
+  delete newObj.key
+  return newObj
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey (object, key) {
+  object = { key };
+}
